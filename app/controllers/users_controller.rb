ec1bd33c433db855fff4ae9be5b782f2601864b6
addo <forison6205@gmail.com>
2019-07-16 06:14:23 -0400

    replicated html with railsy form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-      params.permit(:username, :email, :password,
+      params.require(:user).permit(:username, :email, :password,
                                  :password_confirmation)
     end
 end
