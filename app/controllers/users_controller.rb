1ec7ee64cc70f61c1dddc170b842a6082763ff1b
Samuel <samuelgarciacompanys@gmail.com>
2019-07-16 13:34:58 +0200

    until edit step 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,14 @@ class UsersController < ApplicationController
       end
     end    
 
-    def edit 
-      @user = User.find(params[:id])
+    # def update 
+    #   @user = User.find(params[:id]).update_attributes(user_params)
+    # end
 
-    end 
+     def edit 
+       @user = User.find(params[:id])
+     end 
+
 
     private
 
